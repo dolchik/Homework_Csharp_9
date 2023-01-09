cc7675ba9ae0c01ca74d97b1fbd7710847e206f8
OLGA DRACH <dolchik88@mail.ru>
2023-01-09 16:38:32 +0300

    Сумма элементов между M и N

---
+++ Ex_66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumNumbers(int M, int N)
+{
+    if (M == N) return N;
+    return SumNumbers(M + 1, N) + M;
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//запрашиваем у пользователя число N
+int M = EnterNumber("Введите число M:");
+int N = EnterNumber("Введите число N: ");
+//рекурсия, которая выводит все числа в промежутке от 1 до N
+if (M <= N)
+{
+    int sum = SumNumbers(M, N);
+    Console.WriteLine($"Сумма натуральных элементов от M до N: {sum}");
+}
+else Console.WriteLine("некоректный ввод");
