8bd9597e29b40c4d6cae328debaaae9f30b63c5b
OLGA DRACH <dolchik88@mail.ru>
2023-01-09 12:48:09 +0300

    Программа выводит числа от N до 1 с помощью рекурсии

---
+++ Ex_64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+void PrintNumbers(int number)
+{
+    if (number >= 1)
+    {
+        Console.Write($"{number} ");
+        PrintNumbers(number - 1);
+    }
+}
+
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+//запрашиваем у пользователя число N
+int N = EnterNumber("Введите число N:");
+//рекурсия, которая выводит все числа в промежутке от N до 1
+PrintNumbers(N);
